ddcf8008dcdd5a156a9e1e5e3bce631ef71ad7b2
Norazan <thijs.hendrickx@online.nl>
2016-03-11 11:07:29 +0100

    Friend classes implemented

--- src/clock.cpp
+++ src/clock.cpp
@@ -95,31 +95,6 @@ clock::duration clock::duration::operator%(const clock::duration& rhs) const {
 
 }
 
-
-/*clock::time_stamp& operator+=(clock::time_stamp& lhs, const clock::duration& d) {
-
-}
-
-clock::time_stamp& operator-=(clock::time_stamp& lhs, const clock::duration& d) {
-
-}
-
-clock::time_stamp operator+(const clock::time_stamp& lhs, const clock::duration& rhs) {
-
-}
-
-clock::time_stamp operator-(const clock::time_stamp& lhs, const clock::duration& rhs) {
-
-}
-
-clock::time_stamp operator+(const clock::duration& lhs, const clock::time_stamp& rhs) {
-
-}
-
-std::ostream& operator<<(std::ostream &s, const clock::duration& rhs) {
-
-}*/
-
 //============================================================================================================
 //Class Clock::time_stamp
 //============================================================================================================
@@ -162,24 +137,36 @@ clock::duration clock::time_stamp::operator-(const clock::time_stamp& rhs) const
 //============================================================================================================
 
 clock::time_stamp& operator+=(clock::time_stamp& lhs, const clock::duration& d) {
-
+	lhs.m_time_stamp = lhs.m_time_stamp + d.m_duration;
+	return lhs;
 }
+
 clock::time_stamp& operator-=(clock::time_stamp& lhs, const clock::duration& d) {
-
+	lhs.m_time_stamp = lhs.m_time_stamp - d.m_duration;
+	return lhs;
 }
+
 clock::time_stamp operator+(const clock::time_stamp& lhs, const clock::duration& rhs) {
-
+	return lhs.m_time_stamp + rhs.m_duration;
 }
+
 clock::time_stamp operator-(const clock::time_stamp& lhs, const clock::duration& rhs) {
-
+	return lhs.m_time_stamp - rhs.m_duration;
 }
+
 clock::time_stamp operator+(const clock::duration& lhs, const clock::time_stamp& rhs) {
-
+	return lhs.m_duration + rhs.m_time_stamp;
 }
+
 
 std::ostream & operator<<(std::ostream & s, const clock::duration & rhs) {
-
+	s << rhs.m_duration.count();
+	return s;
 }
-std::ostream & operator<<(std::ostream & s, const clock::time_stamp &rhs) {
 
+std::ostream & operator<<(std::ostream & s, const clock::time_stamp &rhs) {
+	std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(rhs.m_time_stamp.time_since_epoch());
+	std::chrono::seconds sec = std::chrono::duration_cast<std::chrono::seconds>(ms);
+	s << sec.count();
+	return s;
 }

dd8c473f0b91e007b11d25b5199ff60721e83ab2
Aydin <biber.aydin@gmail.com>
2016-03-15 12:29:44 +0100

    Changed return value of const operators

--- src/clock.cpp
+++ src/clock.cpp
@@ -21,33 +21,27 @@ clock::duration::duration(const std::chrono::duration<long, std::micro>& arg) {
 }
 
 const clock::duration& clock::duration::operator+=(const duration &d) {
-	m_duration += d.m_duration;
-	return m_duration;
+	return this->m_duration += d.m_duration;
 }
 
 const clock::duration& clock::duration::operator-=(const duration &d) {
-	m_duration -= d.m_duration;
-	return m_duration;
+	return this->m_duration -= d.m_duration;
 }
 
 const clock::duration& clock::duration::operator*=(const int rhs) {
-	m_duration = m_duration * rhs;
-	return m_duration;
+	return this->m_duration = m_duration * rhs;
 }
 
 const clock::duration& clock::duration::operator/=(const int rhs) {
-	m_duration = m_duration / rhs;
-	return m_duration;
+	return this->m_duration = m_duration / rhs;
 }
 
 const clock::duration& clock::duration::operator%=(const int rhs) {
-	m_duration = m_duration % rhs;
-	return m_duration;
+	return this->m_duration = m_duration % rhs;
 }
 
 const clock::duration& clock::duration::operator%=(const duration &rhs) {
-	m_duration = m_duration % rhs.m_duration;
-	return m_duration;
+	return this->m_duration = m_duration % rhs.m_duration;
 }
 
 bool clock::duration::operator==(const duration & rhs) const {

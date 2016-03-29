f1cc51e1d5622442f3a080f6ca53145a8489f4f3
zehnzen <zehna-berg@live.nl>
2016-03-29 17:25:23 +0200

    google test geschreven van standaards vergelijkingsoperatoren van de Time_stamp klasse + duration diffs en copyconstructor

--- test/test.cpp
+++ test/test.cpp
@@ -134,39 +134,138 @@ TEST(DivideDuration, ConstInt) {
 	time_stamp
 */
 
-TEST(IsEqualTimeStamp, ConstTimeStamp) {
-	clock::time_stamp a = clock::time_stamp::time_stamp(std::chrono::time_point<std::chrono::high_resolution_clock>(10));
-	clock::time_stamp b = clock::time_stamp::time_stamp(std::chrono::time_point<std::chrono::high_resolution_clock>(10));
-	clock::time_stamp c = clock::time_stamp::time_stamp(std::chrono::time_point<std::chrono::high_resolution_clock>(5));
+TEST(ConstructorTimeStamp, Default){ // TODO Moet nog gedaan worden, weet momenteel niet zogoed hoe ik zijn returnwaarde moet testen/printen
+	clock::time_stamp a = clock::time_stamp::time_stamp();
+}
+
+TEST(ConstructorTimeStamp, Copy){
+	clock::time_stamp a = clock::time_stamp::time_stamp(); //Vanaf standaard constructor
+	clock::time_stamp b = a;
 	
-	EXPECT_EQ(a, b);
+	EXPECT_EQ(a, b) << "Copy constructor maakt gelijke objecten";
+}
+
+TEST(IsEqualTimeStamp, ConstTimeStamp) {
+	clock::time_stamp a = clock::time_stamp::time_stamp(); //Vanaf standaard constructor
+	clock::time_stamp b = a;
+	
+	
+	EXPECT_TRUE(a == b) << "Default copy constructor gelijk";
+	
+	std::chrono::high_resolution_clock::time_point t1 = high_resolution_clock::now(); // Vergelijking vanaf voorgedefineerde tijd
+	a = clock::time_stamp::time_stamp(t1);
+	b = clock::time_stamp::time_stamp(t1);
+	
+	EXPECT_TRUE(a == b) << "Equal vanaf time_point constructor";
+	
+	b = clock::time_stamp::time_stamp();
+	EXPECT_FALSE(a == b) << "Returns false voor verschillende timestamps";
 }
 
 TEST(IsNotEqualTimeStamp, ConstTimeStamp) {
-	clock::time_stamp a = clock::time_stamp::time_stamp(std::chrono::time_point<std::chrono::high_resolution_clock>(10));
-	clock::time_stamp b = clock::time_stamp::time_stamp(std::chrono::time_point<std::chrono::high_resolution_clock>(5));
-	clock::time_stamp c = clock::time_stamp::time_stamp(std::chrono::time_point<std::chrono::high_resolution_clock>(10));
+	clock::time_stamp a = clock::time_stamp::time_stamp();
+	int x = 1;
+	for(int i= 0; i< 1000; ++i){
+		x++;
+	}
+	clock::time_stamp b = clock::time_stamp::time_stamp();
 	
-	EXPECT_EQ(a, c);
+	EXPECT_TRUE(a != b) << "2 default constructors ongelijk met forloop ertussen";
+	
+	std::chrono::high_resolution_clock::time_point t1 = high_resolution_clock::now(); // Vergelijking vanaf voorgedefineerde tijd
+	a = clock::time_stamp::time_stamp(t1);
+	b = clock::time_stamp::time_stamp(t1);
+	EXPECT_FALSE(a != b) << "2 gelijke timestamps geven false terug";
 }
 
 TEST(LessThanTimeStamp, ConstTimeStamp) {
+	clock::time_stamp a = clock::time_stamp::time_stamp();
+	int x = 1;
+	for(int i= 0; i< 1000; ++i){
+		x++;
+	}
+	clock::time_stamp b = clock::time_stamp::time_stamp();
+	EXPECT_TRUE(a < b) << "Returns true als a < b";
+	EXPECT_FALSE(b < a) << "Returns false bij b < a";
 	
+	std::chrono::high_resolution_clock::time_point t1 = high_resolution_clock::now(); // Vergelijking vanaf voorgedefineerde tijd
+	a = clock::time_stamp::time_stamp(t1);
+	b = clock::time_stamp::time_stamp(t1);
+	
+	EXPECT_FALSE(a < b) << "Returns false bij gelijke waarde";
 }
 
 TEST(LessOrEqualTimeStamp, ConstTimeStamp) {
+	clock::time_stamp a = clock::time_stamp::time_stamp();
+	int x = 1;
+	for(int i= 0; i< 1000; ++i){
+		x++;
+	}
+	clock::time_stamp b = clock::time_stamp::time_stamp();
+	EXPECT_TRUE(a <= b) << "Returns true als a <= b";
+	EXPECT_FALSE(b <= a) << "Returns false als b <= a";
 	
+	std::chrono::high_resolution_clock::time_point t1 = high_resolution_clock::now(); // Vergelijking vanaf voorgedefineerde tijd
+	a = clock::time_stamp::time_stamp(t1);
+	b = clock::time_stamp::time_stamp(t1);
+	
+	EXPECT_TRUE(a <= b) << "Returns true bij gelijke waarde";
 }
 
 TEST(GreaterThanTimeStamp, ConstTimeStamp) {
+	clock::time_stamp b = clock::time_stamp::time_stamp();
+	int x = 1;
+	for(int i= 0; i< 1000; ++i){
+		x++;
+	}
+	clock::time_stamp a = clock::time_stamp::time_stamp();
+	EXPECT_TRUE(a > b) << "Returns true als a > b";
+	EXPECT_FALSE(b > a) << "Returns false als b > a";
 	
+	std::chrono::high_resolution_clock::time_point t1 = high_resolution_clock::now(); // Vergelijking vanaf voorgedefineerde tijd
+	a = clock::time_stamp::time_stamp(t1);
+	b = clock::time_stamp::time_stamp(t1);
+	
+	EXPECT_FALSE(a > b) << "Returns false bij gelijke waarde";
 }
 
 TEST(GreaterOrEqualTimeStamp, ConstTimeStamp) {
+	clock::time_stamp b = clock::time_stamp::time_stamp();
+	int x = 1;
+	for(int i= 0; i< 1000; ++i){
+		x++;
+	}
+	clock::time_stamp a = clock::time_stamp::time_stamp();
+	EXPECT_TRUE(a >= b) << "Returns true als a >= b";
+	EXPECT_FALSE(b >= a) << "Returns false als b >= a";
 	
+	std::chrono::high_resolution_clock::time_point t1 = high_resolution_clock::now(); // Vergelijking vanaf voorgedefineerde tijd
+	a = clock::time_stamp::time_stamp(t1);
+	b = clock::time_stamp::time_stamp(t1);
+	
+	EXPECT_TRUE(a >= b) << "Returns true bij gelijke waarde";
 }
 
-TEST(RemoveTimeStamp, ConstTimeStamp) {
+TEST(DifferenceTimeStamp, ConstTimeStamp) {
+	std::chrono::high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	int x = 1;
+	for(int i= 0; i< 1000; ++i){
+		x++;
+	}
+	std::chrono::high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	
+	clock::time_stamp a = clock::time_stamp::time_stamp(t1);
+	clock::time_stamp b = clock::time_stamp::time_stamp(t2);
+	
+	clock::duration d1 = clock::duration(std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1));
+	clock::duration	d2 = t2 - t1;
+	EXPECT_EQ(d1 , d2) << "De clock::duration van het verschil tussen de time_stamps is gelijk aan de duration cast";
+	
+	d2 = t1 - t2;
+	EXPECT_NE(d1 , d2) << "De volgorde van aftrekken omdraaien om andere negatieve duration te krijgen";
+	
+	d1 = clock::duration(std::chrono::duration_cast<std::chrono::microseconds>(t1 - t2));
+	EXPECT_EQ(d1 , d2) << "De clock::duration van het verschil tussen de time_stamps is gelijk aan de duration cast geinverteerd";
 	
 }
 

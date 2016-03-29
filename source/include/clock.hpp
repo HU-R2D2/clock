5f600e872335bac12fd6c5c8777fcb8fde8691ff
waila <waila@users.noreply.github.com>
2016-03-29 17:32:49 +0200

    Added doxygen code in class time_stamp.

--- source/include/clock.hpp
+++ source/include/clock.hpp
@@ -46,7 +46,7 @@ class clock::duration {
 		
 		//! The operator +=
 		/*!
-		  This operator adds the current m_duration with the given parameter d.
+		  This operator adds m_duration of current duration with the given parameter d and returns the calculated value. 
 		  \param d is a duration in microseconds which is a long(integer)
 		  \return the calculated value
 		*/
@@ -54,7 +54,7 @@ class clock::duration {
 		
 		//! The operator -=
 		/*!
-		  This operator extracts the current m_duration with the given parameter d.
+		  This operator extracts m_duration of current duration with the given parameter d and returns the calculated value. 
 		  \param d is a duration in microseconds which is a long(integer)
 		  \return the calculated value
 		*/
@@ -62,23 +62,24 @@ class clock::duration {
 		
 		//! The operator *=
 		/*!
-		  This operator multiplies the current m_duration with the given parameter d.
-		  \param rhs is a number which is an integer
+		  This operator multiplies m_duration of current duration with the given parameter d and returns the calculated value. 
+		  \param rhs is a integer
 		  \return the calculated value
 		*/		
 		const duration& operator*=(const int rhs);
 
 		//! The operator /=
 		/*!
-		  This operator devides the current m_duration with the given parameter d.
-		  \param rhs is a number which is an integer
+		  This operator devides m_duration of current duration with the given parameter d and returns the calculated value. 
+		  \param rhs is a integer
 		  \return the calculated value
 		*/		
 		const duration& operator/=(const int rhs);
 		
+		
 		//! The operator == which returns a boolean
 		/*!
-		  This operator checks wether the current m_duration is equal with the given parameter
+		  This operator compares m_duration of current duration with the given parameter rhs
 		  \param rhs is a duration 
 		  \return returns 1 if the comparison is true otherwise 0
 		*/		
@@ -86,7 +87,7 @@ class clock::duration {
 
 		//! The operator != which returns a boolean
 		/*!
-		  This operator checks wether the current m_duration is not equal with the given parameter
+		  This operator compares m_duration of current duration with the given parameter rhs
 		  \param rhs is a duration 
 		  \return returns 1 if the comparison is true otherwise 0
 		*/		
@@ -94,7 +95,8 @@ class clock::duration {
 		
 		//! The operator < which returns a boolean
 		/*!
-		  This operator compares wether the current m_duration is smaller than the given parameter
+	  	  This operator checks if the current m_duration is smaller than the given rhs m_duration
+		  This operator compares m_duration wether the current m_duration is smaller than the given m_duration of rhs
 		  \param rhs is a duration 
 		  \return returns 1 if the comparison is true otherwise 0
 		*/		
@@ -102,7 +104,7 @@ class clock::duration {
 		
 		//! The operator < which returns a boolean
 		/*!
-		  This operator compares wether the current m_duration is smaller or equal than the given parameter
+	  	  This operator checks if the current m_duration is smaller or equal than the given m_duration of rhs
 		  \param rhs is a duration 
 		  \return returns 1 if the comparison is true otherwise 0
 		*/		
@@ -110,7 +112,7 @@ class clock::duration {
 		
 		//! The operator < which returns a boolean
 		/*!
-		  This operator compares wether the current m_duration is bigger than the given parameter
+	  	  This operator checks if the current m_duration is bigger than the ggiven m_duration of rhs
 		  \param rhs is a duration 
 		  \return returns 1 if the comparison is true otherwise 0
 		*/		
@@ -118,7 +120,7 @@ class clock::duration {
 		
 		//! The operator < which returns a boolean
 		/*!
-		  This operator compares wether the current m_duration is bigger or equal than the given parameter
+	  	  This operator checks if the current m_duration is bigger or equal than the given m_duration of rhs
 		  \param rhs is a duration 
 		  \return returns 1 if the comparison is true otherwise 0
 		*/		
@@ -126,7 +128,7 @@ class clock::duration {
 		
 		//! The operator +
 		/*!
-		  This operator adds m_duration of current duration with the given duration rhs. 
+		  This operator adds m_duration of current duration with the given parameter rhs. 
 		  \param rhs is a duration 
 		  \return returns duration which contains the calculated m_duration
 		*/
@@ -134,7 +136,7 @@ class clock::duration {
 		
 		//! The operator -
 		/*!
-		  This operator extracts m_duration of current duration with the given duration rhs. 
+		  This operator extracts m_duration of current duration with the given parameter rhs. 
 		  \param rhs is a duration 
 		  \return returns duration which contains the calculated m_duration
 		*/		
@@ -142,7 +144,7 @@ class clock::duration {
 		
 		//! The operator -
 		/*!
-		  This operator multiplies m_duration of current duration with the given duration rhs. 
+		  This operator multiplies m_duration of current duration with the given parameter rhs. 
 		  \param rhs is an integer 
 		  \return returns duration which contains the calculated m_duration
 		*/		
@@ -150,18 +152,13 @@ class clock::duration {
 		
 		//! The operator /
 		/*!
-		  This operator devides m_duration of current duration with the given duration rhs. 
+		  This operator devides m_duration of current duration with the given parameter rhs. 
 		  \param rhs is an integer 
 		  \return returns duration which contains the calculated m_duration
 		*/		
 		duration operator/(const int rhs) const;
 
-		//! The operator -=
-		/*!
-		  This operator extracts the current m_duration with the given parameter d.
-		  \param d is a duration in microseconds which is a long(integer)
-		  \return the calculated value
-		*/
+
 		friend clock::time_stamp& operator+=(clock::time_stamp& lhs, const clock::duration& d);
 		friend clock::time_stamp& operator-=(clock::time_stamp& lhs, const clock::duration& d);
 		friend clock::time_stamp operator+(const clock::time_stamp& lhs, const clock::duration& rhs);
@@ -191,13 +188,61 @@ class clock::time_stamp {
 		time_stamp(const time_stamp& arg) = default;
 		
 	public: /*Operators*/
+	
+		//! The operator == which returns a boolean 
+		/*!
+	  	  This operator checks if the current time_stamp is equal with the given time_stamp of rhs
+		  \param rhs is a time_stamp 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/
 		bool operator==(const clock::time_stamp& rhs) const;
+		
+		//! The operator != which returns a boolean 
+		/*!
+	  	  This operator checks if the current time_stamp is not equal with the given time_stamp of rhs
+		  \param rhs is a time_stamp 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator!=(const clock::time_stamp& rhs) const;
+		
+		//! The operator < which returns a boolean
+		/*!
+	  	  This operator checks if the current time_stamp is smaller than the given time_stamp of rhs
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator<(const clock::time_stamp& rhs) const;
+		
+		//! The operator <= which returns a boolean
+		/*!
+	  	  This operator checks if the current time_stamp is smaller or equal than the given time_stamp of rhs
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/			
 		bool operator<=(const clock::time_stamp& rhs) const;
+		
+		//! The operator <= which returns a boolean
+		/*!
+	  	  This operator checks if the current time_stamp is bigger than the given time_stamp of rhs
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator>(const clock::time_stamp& rhs) const;
+		
+		//! The operator <= which returns a boolean
+		/*!
+	  	  This operator checks if the current time_stamp is bigger or equal than the given time_stamp of rhs
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator>=(const clock::time_stamp& rhs) const;
 		
+		//! The operator - which returns duration
+		/*!
+		  This operator extracts the m_time_stamp of the current timestamp with the given m_time_stamp and returns a duration
+		  \param rhs is a time_stamp 
+		  \return returns the calculated time as duration
+		*/		
 		clock::duration operator-(const clock::time_stamp& rhs) const;
 
 		friend clock::time_stamp& operator+=(clock::time_stamp& lhs, const clock::duration& d);
@@ -207,12 +252,57 @@ class clock::time_stamp {
 		friend clock::time_stamp operator+(const clock::duration& lhs, const clock::time_stamp& rhs);
 		friend std::ostream& operator<<(std::ostream &s, const clock::time_stamp& rhs);
 };
-	
+
+//! The operator +=
+/*!
+  This operater takes the lhs time_stamp and adds the duration d to it and stores the calculated value back to the lhs.
+  Use this to show the time after adding duration.
+  \param lhs is a time_stamp
+  \param d is a duration
+  \return the time after adding the given duration to it
+*/
 clock::time_stamp& operator+=(clock::time_stamp& lhs, const clock::duration& d);
+
+//! The operator -=
+/*!
+  This operater takes the lhs time_stamp and extracts the duration d to it and stores the calculated value back to the lhs.
+  Use this to show the time after extracting duration.
+  \param lhs is a time_stamp
+  \param d is a duration
+  \return the time after extracting the given duration to it
+*/
 clock::time_stamp& operator-=(clock::time_stamp& lhs, const clock::duration& d);
+
+//! The operator +
+/*!
+  This operater takes the lhs time_stamp and adds the duration d to it.
+  Use this to show the time after adding duration.
+  \param lhs is a time_stamp
+  \param d is a duration
+  \return the time after adding the given duration to it
+*/
 clock::time_stamp operator+(const clock::time_stamp& lhs, const clock::duration& rhs);
+
+//! The operator -
+/*!
+  This operater takes the lhs time_stamp and extracts the duration d to it.
+  Use this to show the time after extracting duration.
+  \param lhs is a duration
+  \param rhs is a time_stamp
+  \return the time after extracting the given duration to it
+*/
 clock::time_stamp operator-(const clock::time_stamp& lhs, const clock::duration& rhs);
+
+//! The operator +
+/*!
+  This operater takes the lhs duration and adds the rhs time_stamp to it.
+  Use this to show the time after adding duration.
+  \param lhs is a duration
+  \param rhs is a time_stamp
+  \return the time after adding the given duration to it
+*/
 clock::time_stamp operator+(const clock::duration& lhs, const clock::time_stamp& rhs);
+
 	
 std::ostream & operator<<(std::ostream & s, const clock::duration & rhs);
 std::ostream & operator<<(std::ostream & s, const clock::time_stamp &rhs);

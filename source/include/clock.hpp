4336f958edb220797e2130b1c9720fbaeebbc8ed
waila <waila@users.noreply.github.com>
2016-03-29 16:24:24 +0200

    Added doxygen code in duration class
    - Friend operaters code & doxygen still missing and needs to be added

--- source/include/clock.hpp
+++ source/include/clock.hpp
@@ -32,7 +32,10 @@ class clock::duration {
 		duration();
 
 		//! Default initialization constructor
-
+		/*!
+		  \param arg duration is a chrono argument which uses the datatype long in microseconds
+		  \return the duration of arg
+		*/
 		duration(const std::chrono::duration<long, std::micro>& arg);
 
 		//! Default copy constructor
@@ -40,28 +43,125 @@ class clock::duration {
 		duration(const duration& arg) = default;
 
 	public: /*Opearators*/
-
+		
+		//! The operator +=
+		/*!
+		  This operator adds the current m_duration with the given parameter d.
+		  \param d is a duration in microseconds which is a long(integer)
+		  \return the calculated value
+		*/
 		const duration& operator+=(const duration& d);
+		
+		//! The operator -=
+		/*!
+		  This operator extracts the current m_duration with the given parameter d.
+		  \param d is a duration in microseconds which is a long(integer)
+		  \return the calculated value
+		*/
 		const duration& operator-=(const duration& d);
+		
+		//! The operator *=
+		/*!
+		  This operator multiplies the current m_duration with the given parameter d.
+		  \param rhs is a number which is an integer
+		  \return the calculated value
+		*/		
 		const duration& operator*=(const int rhs);
+
+		//! The operator /=
+		/*!
+		  This operator devides the current m_duration with the given parameter d.
+		  \param rhs is a number which is an integer
+		  \return the calculated value
+		*/		
 		const duration& operator/=(const int rhs);
-		const duration& operator%=(const int rhs);
-		const duration& operator%=(const duration& rhs);
-
+		
+		//! The operator == which returns a boolean
+		/*!
+		  This operator checks wether the current m_duration is equal with the given parameter
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator==(const duration& rhs) const;
+
+		//! The operator != which returns a boolean
+		/*!
+		  This operator checks wether the current m_duration is not equal with the given parameter
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator!=(const duration& rhs) const;
+		
+		//! The operator < which returns a boolean
+		/*!
+		  This operator compares wether the current m_duration is smaller than the given parameter
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator<(const duration& rhs) const;
+		
+		//! The operator < which returns a boolean
+		/*!
+		  This operator compares wether the current m_duration is smaller or equal than the given parameter
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator<=(const duration& rhs) const;
+		
+		//! The operator < which returns a boolean
+		/*!
+		  This operator compares wether the current m_duration is bigger than the given parameter
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator>(const duration& rhs) const;
+		
+		//! The operator < which returns a boolean
+		/*!
+		  This operator compares wether the current m_duration is bigger or equal than the given parameter
+		  \param rhs is a duration 
+		  \return returns 1 if the comparison is true otherwise 0
+		*/		
 		bool operator>=(const duration& rhs) const;
-
+		
+		//! The operator +
+		/*!
+		  This operator adds m_duration of current duration with the given duration rhs. 
+		  \param rhs is a duration 
+		  \return returns duration which contains the calculated m_duration
+		*/
 		duration operator+(const clock::duration& rhs) const;
+		
+		//! The operator -
+		/*!
+		  This operator extracts m_duration of current duration with the given duration rhs. 
+		  \param rhs is a duration 
+		  \return returns duration which contains the calculated m_duration
+		*/		
 		duration operator-(const clock::duration& rhs) const;
+		
+		//! The operator -
+		/*!
+		  This operator multiplies m_duration of current duration with the given duration rhs. 
+		  \param rhs is an integer 
+		  \return returns duration which contains the calculated m_duration
+		*/		
 		duration operator*(const int rhs) const;
+		
+		//! The operator /
+		/*!
+		  This operator devides m_duration of current duration with the given duration rhs. 
+		  \param rhs is an integer 
+		  \return returns duration which contains the calculated m_duration
+		*/		
 		duration operator/(const int rhs) const;
-		duration operator%(const int rhs) const;
-		duration operator%(const clock::duration& rhs) const;
 
+		//! The operator -=
+		/*!
+		  This operator extracts the current m_duration with the given parameter d.
+		  \param d is a duration in microseconds which is a long(integer)
+		  \return the calculated value
+		*/
 		friend clock::time_stamp& operator+=(clock::time_stamp& lhs, const clock::duration& d);
 		friend clock::time_stamp& operator-=(clock::time_stamp& lhs, const clock::duration& d);
 		friend clock::time_stamp operator+(const clock::time_stamp& lhs, const clock::duration& rhs);

feca19b1164c827d6fd10834347d2bb35b353e4f
Norazan <thijs.hendrickx@online.nl>
2016-05-12 12:33:58 +0200

    Added Doxyfile

--- docs/html/search/all_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['clock',['Clock',['../class_clock.html',1,'']]],
-  ['clock_2ecpp',['Clock.cpp',['../_clock_8cpp.html',1,'']]],
-  ['clock_2ehpp',['Clock.hpp',['../_clock_8hpp.html',1,'']]]
-];
--- docs/html/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['clock_2ecpp',['Clock.cpp',['../_clock_8cpp.html',1,'']]],
-  ['clock_2ehpp',['Clock.hpp',['../_clock_8hpp.html',1,'']]]
-];
